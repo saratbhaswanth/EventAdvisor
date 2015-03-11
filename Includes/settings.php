2e4058a1347723e9a2be71fa38aac54ba5169a51
ravindra-msp <ravindra@mysmartprice.com>
2015-03-12 00:26:39 +0530

    settings

--- Includes/settings.php
+++ Includes/settings.php
@@ -4,5 +4,5 @@ ini_set("error_reporting", E_ERROR | E_PARSE);
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASSWORD","root");
-define("DB_NAME","EventAdvisor");
+define("DB_NAME","eventadvisor");
 ?>
--- Login/ValidateLogin.php
+++ Login/ValidateLogin.php
@@ -6,13 +6,13 @@ if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $connection->connect_error);
     exit();
 }
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $connection->real_escape_string($_POST['username']);
+$password = $connection->real_escape_string($_POST['password']);
 $password = md5($password);
-$select_query = "SELECT * FROM login_credentails WHERE username='$username' AND password='$password'";
+$select_query = "SELECT * FROM admin_login WHERE username='$username' AND password='$password'";
 $select_result = $connection->query($select_query);
 if($select_result->num_rows >=1){
-	echo "SUCESS";
+	echo "SUCCESS";
 }
 else {
 	die("Failed");
