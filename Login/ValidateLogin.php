ee1333456876d15bbfbe299a98b4370ae9809127
ravindra-msp <ravindra@mysmartprice.com>
2015-03-11 16:59:11 +0530

    Validate login

---
+++ Login/ValidateLogin.php
@@ -0,0 +1,21 @@
+<?php
+include_once "settings.php";
+
+$connection=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $connection->connect_error);
+    exit();
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
+$password = md5($password);
+$select_query = "SELECT * FROM login_credentails WHERE username='$username' AND password='$password'";
+$select_result = $connection->query($select_query);
+if($select_result->num_rows >=1){
+	echo "SUCESS";
+}
+else {
+	die("Failed");
+}
+$connection->close();
+?>
