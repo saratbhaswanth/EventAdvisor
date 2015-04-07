540600a873002264a4917a3ab61690821850e6c5
Sarat Bhaswanth <sarat.bhaswanth@gmail.com>
2015-04-07 16:53:50 +0530

    Updated Jquery search function

---
+++ category/wedding/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Weddings | Event Advisor</title>
+        <meta charset="utf-8">
+        <link href="../../css/stylesheet.css" rel='stylesheet' type='text/css' />
+
+        <script src="../../scripts/jquery-2.1.3.min.js"></script>
+        <link href="http://fonts.googleapis.com/css?family=Lato:400,300,100,300italic" rel="stylesheet" type="text/css">
+
+        <script type=" text/javascript " src="../scripts/application.js"></script>
+        <meta name="viewport " content="width=device-width, initial-scale=1 ">
+    </head>
+
+    <body>
+        <?php echo file_get_contents( "../../header.php"); ?>
+        <div class="body-index">
+            <section class="main-categories ">
+                <div class="main-categories-heading ">
+                    Weddings
+                </div>
+                <div class="group ">
+                    <div class="sub-group category-1 " name="category-1 ">
+                        <div class="group-title ">Subcategory-1<span class="group-more ">
+                            </span>
+                        </div>
+
+                        <div class="col span_1_of_4 ">
+                            1 of 4
+                        </div>
+                        <div class="col span_1_of_4 ">
+                            1 of 4
+                        </div>
+                        <div class="col span_1_of_4 ">
+                            1 of 4
+                        </div>
+                        <div class="col span_1_of_4 ">
+                            1 of 4
+                        </div>
+                    </div>
+                    <div class="sub-group category-2 " name="category-2 ">
+                        <div class="group-title ">Subcategory-2<span class="group-more ">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="sub-group category-3 " name="category-3">
+                        <div class="group-title ">Subcategory-3<span class="group-more ">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="sub-group category-4 " name="category-4">
+                        <div class="group-title ">Subcategory-4<span class="group-more ">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                    <div class="col span_1_of_4 ">
+                        1 of 4
+                    </div>
+                </div>
+                </div>
+            </section>
+        </div>
+    <?php echo file_get_contents( "../../footer.php"); ?>
+    </body>
+
+</html>
